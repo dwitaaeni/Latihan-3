1f6fbad5892a53d598a8314df6405380ba32706f
dwitaaeni <witaaeni06@gmail.com>
2021-10-28 13:54:32 +1100

    File Pertama

---
+++ Program_Lingkaran.py
@@ -0,0 +1,10 @@
+print("Program Menghitung Luas dan Keliling Lingkaran  ")
+print("================================================")
+r = float (input("masukan jari-jari lingkaran : "))
+
+phi = 3.14
+luas = phi*r*r
+keliling = 2*phi*r
+
+print ("luas lingkaran :",luas);
+print ("keliling lingkran :",keliling);
